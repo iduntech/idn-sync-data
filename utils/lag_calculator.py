d0418f63855ac4973c529a50d5ee74cd8c5a4f52
Waddaben <waddabenjamin@gmail.com>
2023-08-23 16:52:14 +0200

    made throughout cutting code function

--- utils/lag_calculator.py
+++ utils/lag_calculator.py
@@ -144,30 +144,80 @@ def prepare_idun_data(idun_raw_data, config):
     )
     return idun_highpassed_data, idun_filtered_data, idun_time_stamps
 
-def polynomial_regression_on_lag(cleaned_fine_lag_arr,polynomial_degree):
+
+def polynomial_regression_on_lag(cleaned_fine_lag_arr, polynomial_degree):
     original_raw_lag = copy.deepcopy(cleaned_fine_lag_arr)
     x_axis_lag = np.arange(len(cleaned_fine_lag_arr)).reshape(-1, 1)
     x_axis_lag_copy = x_axis_lag.copy()
-    
+
     # find where y is not nan
     not_nan_idx = np.where(~np.isnan(original_raw_lag))[0]
     original_raw_lag = original_raw_lag[not_nan_idx]
     x_axis_lag = x_axis_lag[not_nan_idx]
-    
+
     # Transform the features to 2nd degree polynomial features
     poly = PolynomialFeatures(degree=polynomial_degree)
     X_poly = poly.fit_transform(x_axis_lag)
-    
+
     # Create a LinearRegression model and fit it to the polynomial features
     reg = LinearRegression().fit(X_poly, original_raw_lag)
-    
+
     # Predict values
     X_new_poly = poly.transform(x_axis_lag_copy)
     linear_regression_lag = reg.predict(X_new_poly)
-    
+
     return linear_regression_lag
 
 
+def cut_throughout_data(
+    prodigy_cut_data, prodigy_base_cut_df, lag_positions, cumulative_lags
+):
+    dataset2 = copy.deepcopy(prodigy_cut_data)
+    base_dataset2 = copy.deepcopy(prodigy_base_cut_df)
+    dataset2_list = dataset2.tolist()
+
+    for i in range(len(lag_positions)):
+        start_index = lag_positions[i]
+        # Calculate how many elements to replace with np.nan
+        if i == 0:
+            n_replace = int(cumulative_lags[i])
+        else:
+            n_replace = int(cumulative_lags[i] - cumulative_lags[i - 1])
+
+        for j in range(n_replace):
+            if start_index + j < len(dataset2_list):
+                dataset2_list[start_index + j] = np.nan
+                base_dataset2.iloc[start_index + j] = np.nan
+
+    # Convert back to numpy array and remove np.nan values
+    adjusted_dataset2 = np.array(dataset2_list)
+    adjusted_dataset2 = adjusted_dataset2[~np.isnan(adjusted_dataset2)]
+
+    # Drop rows in base_dataset2 that contain NaN values
+    base_dataset2 = base_dataset2.dropna()
+    return adjusted_dataset2, base_dataset2
 
 
+def cut_at_end(
+    prodigy_adjusted_final_arr,
+    prodigy_adjusted_base_final_df,
+    idun_cut_data,
+    idun_base_cut_data,
+):
+    # Cut from the end of the longer dataset
+    if len(prodigy_adjusted_final_arr) > len(idun_cut_data):
+        prodigy_adjusted_final_arr = prodigy_adjusted_final_arr[: len(idun_cut_data)]
+        prodigy_adjusted_base_final_df = prodigy_adjusted_base_final_df[
+            : len(idun_cut_data)
+        ].reset_index(drop=True)
 
+    else:
+        idun_cut_data = idun_cut_data[: len(prodigy_adjusted_final_arr)]
+        idun_base_cut_data = idun_base_cut_data[: len(prodigy_adjusted_final_arr)]
+
+    return (
+        prodigy_adjusted_final_arr,
+        prodigy_adjusted_base_final_df,
+        idun_cut_data,
+        idun_base_cut_data,
+    )
